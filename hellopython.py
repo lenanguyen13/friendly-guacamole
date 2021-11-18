2d85a8fdc6a46084c74c44949189787f5996a595
lenanguyen13 <93352336+lenanguyen13@users.noreply.github.com>
2021-11-18 23:15:55 +0000

    added questions

--- hellopython.py
+++ hellopython.py
@@ -1,1 +1,13 @@
 print("hello, world")
+name = input("what is your name? ")
+print("hello there " + name)
+age = int(input("how old are you?" ))
+if(age < 17):
+  print("not old enough to enter, the movie's rated r")
+else:
+  print("come on in!")
+age = int(input("can i see your id?"))
+if
+  print("if you're going to have a fake one at least make it somewhat believable!")
+else: 
+  print("that will be $25.")
